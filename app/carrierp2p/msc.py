06a56edfa78aeaa662d8ca95f72f181a840b4437
Neck Chi <neck.chi@kuehne-nagel.com>
2023-08-08 17:28:18 +0000

    Change A Token Logic

--- app/carrierp2p/msc.py
+++ app/carrierp2p/msc.py
@@ -3,7 +3,7 @@ import base64
 from datetime import datetime, timedelta, timezone
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.backends import default_backend
-from app.carrierp2p.helpers import call_client
+from app.routers.router_config import HTTPXClientWrapper
 
 
 async def get_msc_token(client, oauth: str, aud: str, rsa: str, msc_client: str, msc_scope: str, msc_thumbprint: str):
@@ -21,7 +21,7 @@ async def get_msc_token(client, oauth: str, aud: str, rsa: str, msc_client: str,
                     'client_assertion_type': 'urn:ietf:params:oauth:client-assertion-type:jwt-bearer',
                     'grant_type': 'client_credentials', 'client_assertion': encoded}
     headers: dict = {'Content-Type': 'application/x-www-form-urlencoded'}
-    response = await anext(call_client(client=client,method='POST',url=oauth, headers=headers, data=params))
+    response = await anext(HTTPXClientWrapper.call_client(client=client,method='POST',url=oauth, headers=headers, data=params))
     response_token = response.json()
     yield response_token['access_token']
 
@@ -36,8 +36,7 @@ async def get_msc_p2p(client, url: str, oauth: str, aud: str, pw: str, msc_clien
 
     token = await anext( get_msc_token(client=client, oauth=oauth, aud=aud, rsa=pw, msc_client=msc_client, msc_scope=msc_scope,msc_thumbprint=msc_thumbprint))
     headers: dict = {'Authorization': f'Bearer {token}'}
-    response = await anext(
-        call_client(client=client, method='GET', url=url, params=params, headers=headers))
+    response = await anext(HTTPXClientWrapper.call_client(client=client, method='GET', url=url, params=params, headers=headers))
 
     ## Performance Enhancement - No meomory is used:async generator object - schedules
     async def schedules():
