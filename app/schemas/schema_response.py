3a892dabc28fc987e0eded8651fa86e3c27b42a0
Neck Chi <neck.chi@kuehne-nagel.com>
2023-08-31 22:08:31 +0000

    Aligned with the new version of Pydantic.Performance Improved 2000%

--- app/schemas/schema_response.py
+++ app/schemas/schema_response.py
@@ -1,40 +1,28 @@
 import logging
 from uuid import UUID
 from datetime import datetime
-from pydantic import BaseModel, Field, validator, PositiveInt, root_validator
+from typing_extensions import Annotated
+from pydantic import BaseModel, Field, PositiveInt,field_validator,model_validator
+from pydantic.functional_serializers import PlainSerializer
 from .schema_request import CarrierCode
-
-
-# def check_sequence(value: datetime, values: dict) -> datetime:
-#     if (value - values['etd']).days < 0:
-#         raise ValueError('ETA must be equal and greater than ETD')
-#     return value
 
 def convert_datetime_to_iso_8601(dt: datetime) -> str:
     return dt.strftime('%Y-%m-%dT%H:%M:%S')
 
-
-def check_etd_eta(cls, v) -> datetime:
-    """
-    Reusable validator for etd/eta
-    """
-    if v['etd'] <= v['eta']:
-        if v['eta'] >= v['etd']:
-            return v
-    logging.error('ETA must be equal to or greater than ETD.vice versa')
-    raise ValueError('ETA must be equal to or greater than ETD.vice versa')
-
+datetime = Annotated[
+    datetime, PlainSerializer(convert_datetime_to_iso_8601, return_type=str, when_used='json')
+]
 
 class PointFrom(BaseModel):
     locationName: str | None = Field(max_length=100, default=None, example='Hong Kong')
-    locationCode: str = Field(max_length=5, title="Port Of Discharge", example='HKHKG', regex=r"[A-Z]{2}[A-Z0-9]{3}")
+    locationCode: str = Field(max_length=5, title="Port Of Discharge", example='HKHKG', pattern =r"[A-Z]{2}[A-Z0-9]{3}")
     terminalName: str | None = Field(max_length=100, default=None, example='HONG KONG INTL TERMINAL (HIT4)')
     terminalCode: str | None = Field(default=None, example='HIT4')
 
 
 class PointTo(BaseModel):
     locationName: str | None = Field(max_length=100, default=None, example='Hamburg')
-    locationCode: str = Field(max_length=5, title="Port Of Discharge", example='DEHAM', regex=r"[A-Z]{2}[A-Z0-9]{3}")
+    locationCode: str = Field(max_length=5, title="Port Of Discharge", example='DEHAM', pattern =r"[A-Z]{2}[A-Z0-9]{3}")
     terminalName: str | None = Field(max_length=100, default=None, example='HHLA CTT')
     terminalCode: str | None = Field(default=None, example='DEHAMCTT')
 
@@ -50,18 +38,20 @@ class Cutoff(BaseModel):
 
 class Transportation(BaseModel):
     transportType: str = Field(description='e.g:Vessel,Barge,Feeder,Truck,Rail,Truck / Rail,Intermodal', example='Vessel')
-    transportName: str | None = Field(title='Vehicle Type', description='e.g:VesselName', example='ISEACO WISDOM')
-    referenceType: str | None = Field(title='Reference Type', description='e.g:IMO', example='IMO')
-    reference: str | None = Field(title='Reference Value', description='e.g:Vessel IMO Code', example='9172301')
+    transportName: str | None = Field(title='Vehicle Type', description='e.g:VesselName', example='ISEACO WISDOM',default=None)
+    referenceType: str | None = Field(title='Reference Type', description='e.g:IMO', example='IMO',default=None)
+    reference: int | str | None = Field(title='Reference Value', description='e.g:Vessel IMO Code', example='9172301',default=None)
 
-    @root_validator(pre=True)
-    def check_reference_type_or_reference(cls, v: str):
-        if ('reference' in v and 'referenceType' in v) or ('reference' not in v and 'referenceType' not in v):
-            return v
+    @model_validator(mode = 'after')
+    def check_reference_type_or_reference(self) -> 'Transportation':
+        reference = self.reference
+        reference_type = self.referenceType
+        if (reference_type is not None and reference is not None ) or (reference_type is None and reference is None):
+            return self
         logging.error(' Either both of reference type and reference existed or both are not existed ')
         raise ValueError(' Either both of reference type and reference existed or both are not existed ')
 
-    @validator('transportType')
+    @field_validator('transportType')
     def check_transport_type(cls, v: str) -> str:
         if v not in ('Vessel', 'Barge', 'Feeder', 'Truck', 'Rail','Truck / Rail', 'Intermodal'):
             logging.error('must contain at least one of transport type')
@@ -80,7 +70,6 @@ class Service(BaseModel):
 
 
 class Leg(BaseModel):
-    # voyageCaller:str | UUID = Field(description='Generate UUID based on scac,vesselImo,voyageNum,serviceCode(Optional)', example='27d23af3-36be-57ce-9dbf-7813e672076c')
     pointFrom: PointFrom = Field(description="This could be point/port")
     pointTo: PointTo = Field(description="This could be point/port")
     etd: datetime
@@ -91,24 +80,39 @@ class Leg(BaseModel):
     transportations: Transportation | None
     voyages: Voyage | None = Field(default=None, title="Voyage Number.Keep in mind that voyage number is not mandatory")
     services: Service | None = Field(default=None, title="Service Loop")
-    _check_etd_eta = root_validator(pre=True, allow_reuse=True)(check_etd_eta)
+    # _check_etd_eta = root_validator(pre=True, allow_reuse=True)(check_etd_eta)
 
+    @model_validator(mode='after')
+    def check_reference_type_or_reference(self) -> 'Leg':
+        etd = self.etd
+        eta = self.eta
+        if eta >= etd  or etd <= eta:
+            return self
+        logging.error('ETA must be equal  or greater than ETD.vice versa')
+        raise ValueError('ETA must be equal  or greater than ETD.vice versa')
 
 class Schedule(BaseModel):
     scac: CarrierCode = Field(max_length=4, title="Carrier Code", description="This is SCAC.It must be 4 characters",
                               example="MAEU")
-    pointFrom: str = Field(max_length=5, title="First Port Of Loading", example='HKHKG', regex=r"[A-Z]{2}[A-Z0-9]{3}")
-    pointTo: str = Field(max_length=5, title="Last Port Of Discharge", example='DEHAM', regex=r"[A-Z]{2}[A-Z0-9]{3}")
+    pointFrom: str = Field(max_length=5, title="First Port Of Loading", example='HKHKG', pattern =r"[A-Z]{2}[A-Z0-9]{3}")
+    pointTo: str = Field(max_length=5, title="Last Port Of Discharge", example='DEHAM', pattern =r"[A-Z]{2}[A-Z0-9]{3}")
     etd: datetime
     eta: datetime
-    cyCutOffDate: datetime | None
-    docCutOffDate: datetime | None
-    vgmCutOffDate: datetime | None
+    cyCutOffDate: datetime | None = Field(default= None)
+    docCutOffDate: datetime | None = Field(default= None)
+    vgmCutOffDate: datetime | None = Field(default= None)
     transitTime: int = Field(ge=0, alias='transitTime', title="Schedule Transit Time",
                              description="Transit Time on Schedule Level")
     transshipment: bool = Field(title="Is transshipment?")
     legs: list[Leg] = Field(default_factory=list)
-    _check_etd_eta = root_validator(pre=True, allow_reuse=True)(check_etd_eta)
+    @model_validator(mode='after')
+    def check_reference_type_or_reference(self) -> 'Schedule':
+        etd = self.etd
+        eta = self.eta
+        if eta >= etd  or etd <= eta:
+            return self
+        logging.error('ETA must be equal or greater than ETD.vice versa')
+        raise ValueError('ETA must be equal or greater than ETD.vice versa')
 
 
 class Product(BaseModel):
@@ -116,20 +120,16 @@ class Product(BaseModel):
                             example='27d23af3-36be-57ce-9dbf-7813e672076c')
     origin: str = Field(max_length=5, title="Origin ",
                         description="This is the origin of country presented in UNECE standard", example="HKHKG",
-                        regex=r"[A-Z]{2}[A-Z0-9]{3}")
+                        pattern =r"[A-Z]{2}[A-Z0-9]{3}")
     destination: str = Field(max_length=5, title="Origin ",
                              description="This is the origin of country presented in UNECE standard ", example="DEHAM",
-                             regex=r"[A-Z]{2}[A-Z0-9]{3}")
+                             pattern =r"[A-Z]{2}[A-Z0-9]{3}")
     noofSchedule: PositiveInt = Field(title='Number Of Schedule', example=1)
     schedules: list[Schedule] | None = Field(default=None, title='Number Of Schedules',
                                              description="The number of p2p schedule offered by carrier")
-
-    class Config:
-        json_encoders = {
-            datetime: convert_datetime_to_iso_8601
-        }
 
 
 class Error(BaseModel):
     id: UUID
     error: str
+
