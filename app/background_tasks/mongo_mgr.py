c23582050164426f097b01e47d75649a2b5b9b1b
Michal Zychowicz <external.michal.zychowicz@kuehne-nagel.com>
2023-10-02 08:48:30 +0000

    mongo_mgr.py edited online with Bitbucket

--- app/background_tasks/mongo_mgr.py
+++ app/background_tasks/mongo_mgr.py
@@ -23,8 +23,8 @@ class MongoDBsetting:
     async def insert(self, result: dict):
         utc_timestamp = datetime.datetime.utcnow()
         try:
-            # self.collection.create_index("productid", unique = True)
-            # self.collection.create_index("expiry",expireAfterSeconds = 60 * 60 * 12)
+            self.collection.create_index("productid", unique = True)
+            self.collection.create_index("expiry",expireAfterSeconds = 60 * 60 * 12)
             await self.collection.insert_one(dict(result, **{'expiry': utc_timestamp}))
             logging.info('Background Task:Cached the schedules into P2P schedule collection ')
         except Exception as insert_db:
