338514a3238a238f634e67e50b7a9651e9231802
jaime.franco <jaime.franco@kuehne-nagel.com>
2024-04-08 10:32:27 +0100

    clean code

--- cicd/variables.tf
+++ cicd/variables.tf
@@ -23,7 +23,8 @@ variable "image" {
   default = "p2p-schedule-api-of-carriers"
 }
 variable "project_name" {
-  type = string
+  type        = string
+  description = "Project name"
 }
 variable "project_name_abreb" {
   type = string
@@ -36,53 +37,63 @@ variable "sg_outboud_rules_cidrs" {
 }
 # ECS Service
 variable "ecs_service_desired_count" {
-  type = number
+  type        = number
+  description = "ECS autosacling desired capacity"
 }
 # Auto Scaling settings
 variable "autoscaling_settings_min_capacity" {
-  type = number
+  type        = number
+  description = "ECS autosacling min capacity"
 }
 variable "autoscaling_settings_max_capacity" {
-  type = number
+  type        = number
+  description = "ECS autosacling max capacity"
 }
 variable "autoscaling_settings_cpu_target_value" {
-  type = number
+  type        = number
+  description = "ECS autosacling cpu target"
 }
 variable "autoscaling_settings_memory_target_value" {
-  type = number
+  type        = number
+  description = "ECS autosacling memory target"
 }
 variable "autoscaling_settings_scale_out_cooldown" {
-  type = number
+  type        = number
+  description = "ECS autosacling ou cooldown"
 }
 variable "autoscaling_settings_scale_in_cooldown" {
-  type = number
+  type        = number
+  description = "ECS autosacling in cooldown"
 }
 # ECS Task
 variable "ecs_task_memory" {
-  type = number
+  type        = number
+  description = "ECS service memory"
 }
 variable "ecs_task_cpu" {
-  type = number
+  type        = number
+  description = "ECS service cpu"
 }
 variable "ecs_task_log_retention_days" {
-  type = number
+  type        = number
+  description = "ECS task log retention in days"
 }
 variable "ecs_task_container_definitions_cpu" {
-  type = number
+  type        = number
+  description = "ECS task cpu"
 }
 variable "ecs_task_container_definitions_memory" {
-  type = number
+  type        = number
+  description = "ECS task container memory"
 }
 variable "ecs_task_container_definitions_image" {
-  type = string
-}
-# LB Listner rule
-variable "lb_listener_arn" {
-  type = string
+  type        = string
+  description = "ECS task image name"
 }
 # Secret Configuration
 variable "api_secret_config" {
-  type = string
+  type        = string
+  description = "Secret for app environment variables configuration"
 }
 variable "static_variables" {
   type        = map(string)
